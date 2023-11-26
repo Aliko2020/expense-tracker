6dda4b107be162a706abde001f03e26a9522bae9
rootgh <95168267+Aliko2020@users.noreply.github.com>
2023-11-26 14:35:07 -0800

    ggg

--- src/components/History.js
+++ src/components/History.js
@@ -1,17 +1,18 @@
 import React, { useState } from 'react';
 
 export const History = ({ transactions }) => {
+  console.log(transactions);
   return (
     <div className='history-m-container'>
       <h4>History</h4>
       <hr />
       <div className='history-container'>
-        {transactions.map((transaction, index) => (
+        {transactions.length? transactions.map((transaction, index) => (
           <div className='history' key={index}>
             <p>{transaction.description}</p>
             <p>Ghc <span>{transaction.amount}</span></p>
           </div>
-        ))}
+        )) : <div className='history'><p>History is empty</p></div> }
       </div>
     </div>
   );
